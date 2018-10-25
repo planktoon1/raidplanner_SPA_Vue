bf86e8b65c7e1167e92e456672f492267c60277e
Frederik Oestergaard <fkoefkoe@live.dk>
2018-10-25 20:34:21 +0200

    Importing other project to start from

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+const posts = require('./routes/api/posts');
+app.use('/api/posts', posts);
+
+// Handle production
+if(process.env.NODE_ENV === 'production') {
+    // Static folder
+    app.use(express.static(__dirname + '/public/'));
+
+    // Handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
